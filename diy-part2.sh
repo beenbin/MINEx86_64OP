b0dcac479b07a6207496b3bf4d135ddd7076e6b6
Bingo <61998928+beenbin@users.noreply.github.com>
2020-07-03 01:07:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,12 +9,13 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.9/g' package/base-files/files/bin/config_generate
 #
 git clone https://github.com/beenbin/passwall652.git package/passwall
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/beenbin/OpenClash.git package/Openclash
-#git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+#git clone https://github.com/beenbin/OpenClash.git package/Openclash
+git clone -b master https://github.com/vernesong/OpenClash.git package/Openclash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #冲突编译失败git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
